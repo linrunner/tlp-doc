1b05f1d1ec90ef8009abec0d3a49c3ff6bf88828
Thomas Koch <linrunner@gmx.net>
2023-10-13 19:18:41 +0200

    Revert "conf.py: add sphinx-design, FontAwesome"
    This reverts commit cce03081f96a5aa0903c2df37ed23ed9019b0b3a.
    
    Rationale: DSGVO

--- conf.py
+++ conf.py
@@ -39,7 +39,6 @@ release = '1.6'
 # ones.
 extensions = [
     'sphinx_sitemap',
-    'sphinx_design',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -94,7 +93,6 @@ html_static_path = ['_static']
 # Custom styles
 html_css_files = [
     'custom.css',
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
 ]
 
 # Custom sidebar templates, must be a dictionary that maps document names
