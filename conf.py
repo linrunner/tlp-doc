18be60be3338b765990f9e1b862660cfea48eb62
Thomas Koch <linrunner@gmx.net>
2023-12-15 10:42:35 +0100

    Remove sitemap (hopefully only temporary)

--- conf.py
+++ conf.py
@@ -38,7 +38,6 @@ release = '1.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_sitemap',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
