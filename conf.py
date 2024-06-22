6df94f39245837e937656323ff0827cc88c490a3
Thomas Koch <linrunner@gmx.net>
2024-06-22 16:23:08 +0200

    conf.py: 1.6.1, (c) 2024

--- conf.py
+++ conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'TLP'
-copyright = '2023, linrunner. Licensed under the CC BY-NC-SA'
+copyright = '2024, linrunner. Licensed under the CC BY-NC-SA'
 author = 'Thomas Koch'
 
 # The short X.Y version
-version = '1.6'
+version = '1.6.1'
 # The full version, including alpha/beta/rc tags
-release = '1.6'
+release = '1.6.1'
 
 # -- General configuration ---------------------------------------------------
 
